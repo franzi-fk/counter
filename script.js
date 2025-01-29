e6eb80d7f45cb421db1742b1e324281f0517f7ca
Franziska Kiel <kiel.franziska@outlook.com>
2025-01-29 15:06:29 +0100

    fixes early reset + refactor

--- script.js
+++ script.js
@@ -9,20 +9,23 @@ let count = 0;
 
 function reset() {
   count = 0;
-  label.innerText = count;
-  root.style.setProperty("--counter", "0%");
+  renderCount();
   button.blur(); // removes focus from button, so when pressing space or enter after clicking on reset the button does not get clicked
 }
 
 function countUp() {
   count += 1;
-  label.innerText = count;
+  renderCount();
 
-  if (count % 100 === 0) {
-    root.style.setProperty("--counter", "0%");
-  } else {
-    root.style.setProperty("--counter", `${count % 100}%`); // accesses and sets the --counter in CSS
+  let cssCount = count.toString();
+  if (cssCount.length > 2 && count % 100 != 0) {
+    cssCount = cssCount.slice(-2);
   }
+  root.style.setProperty("--counter", cssCount + "%");
+}
+
+function renderCount() {
+  label.innerText = count;
 }
 
 // Click on main -> count up
@@ -30,9 +33,7 @@ main.addEventListener("click", countUp);
 
 // Press enter or space -> count up
 document.addEventListener("keydown", function (event) {
-  if (event.key === " " || event.key === "Enter") {
-    countUp();
-  }
+  if (event.code == "Enter" || event.code === "Space") countUp();
 });
 
 // Click reset button -> reset counter
